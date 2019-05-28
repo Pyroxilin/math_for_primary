77120532a89919a565567b807561dc4d4f257a73
vizvamitra <vizvamitra@gmail.com>
2019-05-28 23:58:30 +0300

    refactor task-set, add some tasks && some dynamics

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -3,5 +3,9 @@ class LessonsController < ApplicationController
   end
 
   def show
+    @tasks_count = Task.all.count
+
+    task_id = params.fetch(:tid, 0).to_i
+    @task = Task.find(task_id) || Task.find(0)
   end
 end
