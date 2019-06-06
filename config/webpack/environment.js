1317945a4c30e60936b772ee832a23fa983696e2
vizvamitra <vizvamitra@gmail.com>
2019-06-06 19:29:36 +0300

    fix goddamn icons

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -7,4 +7,9 @@ environment.plugins.append('Provide', new webpack.ProvidePlugin({
   Popper: ['popper.js', 'default']
 }))
 
+const CopyPlugin = require('copy-webpack-plugin');
+environment.plugins.append('Copy', new CopyPlugin([
+  { from: 'node_modules/@fortawesome/fontawesome-free/webfonts', to: './webfonts' },
+]))
+
 module.exports = environment
