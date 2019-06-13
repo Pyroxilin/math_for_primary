6b7f71470a47651ba00514f1e2f9fcafb85762fd
vizvamitra <vizvamitra@gmail.com>
2019-06-13 19:04:43 +0300

    fix task lookup during exam

--- app/controllers/task_sets_controller.rb
+++ app/controllers/task_sets_controller.rb
@@ -3,7 +3,7 @@ class TaskSetsController < ApplicationController
 
   def show
     task_set = TaskSet.find(params.fetch(:id))
-    @tasks = task_set.tasks.map { |task| { id: task[:id], status: nil } }
+    @tasks = task_set.tasks.map { |task| { id: task.id, status: nil } }
 
     task_id = params.fetch(:tid, 0).to_i
     @current_task = task_set.tasks.find { |t| t.id == task_id } || task_set.tasks.first
