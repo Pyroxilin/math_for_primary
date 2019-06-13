7c3d561f3191a4925ae221a49e4f3e93a3e1fd39
vizvamitra <vizvamitra@gmail.com>
2019-06-14 01:37:28 +0300

    add missing theory

--- app/javascript/test.js
+++ app/javascript/test.js
@@ -1,10 +1,12 @@
 $(document).on('turbolinks:load', function() {
-  console.log($)
   $(window).keypress(function(e){
     var key = e.keyCode ? e.keyCode : e.which;
     if (key == 83) {// shift+s
-      document.querySelector('.lorem-ipsum').classList.toggle('d-none')
-      document.querySelector('.absolute-truth').classList.toggle('d-none')
+      var lorem = document.querySelector('.lorem-ipsum')
+      var truth = document.querySelector('.absolute-truth')
+
+      if (lorem) { lorem.classList.toggle('d-none') }
+      if (truth) { truth.classList.toggle('d-none') }
     }
   })
 })
