2075a91faee11c2af683ca569236bd12c17744f6
vizvamitra <vizvamitra@gmail.com>
2019-06-16 20:05:24 +0300

    some more layout fixes && close access to other user`s examinees info

--- app/controllers/exams/examinees_controller.rb
+++ app/controllers/exams/examinees_controller.rb
@@ -1,8 +1,10 @@
 module Exams
   class ExamineesController < ApplicationController
+    before_action :authenticate_user!
+
     def show
       @examinee = Examinee.find(params[:id])
-      @exam = @examinee.exam
+      @exam = current_user.exams.find(@examinee.exam_id)
       @tasks = @exam.task_set.tasks
       @submissions = @examinee.submissions
     end
